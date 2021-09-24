b1cd72362977fcb09cd64982e0afc75544174cfd
Tim Bikbaev <tbikb@DESKTOP-A7C8FAP.localdomain>
2021-09-24 19:56:16 +0300

    mail forwarded to fe

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Timur Bikbaev</h1>
+    <h2>DevOps Engineer</h2>
+    <img id="photo" alt="Tim logo" src="./assets/avatar.jpg">
+    <contacts mail="t.bikbaev@gmail.com"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import contacts from './components/contacts.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    contacts
   }
 }
 </script>
@@ -21,8 +23,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+img#photo {
+  width: 100px;
+  height: auto;
+  position: absolute;
+  top: 1%;
+  right: 1%;
 }
 </style>
---
+++ src/components/contacts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="contacts">
+     <p>Contacts</p>
+        <ul>
+            <li>mail:{{ mail }}</li>
+            <li>skype: tim.bikbaev </li>
+            <li>phone: +380671014882</li>
+            <li>telegram: @t_bikbaev</li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contacts',
+  props: {
+    mail: String
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+/* ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+a {
+  color: #42b983;
+}
+</style>
